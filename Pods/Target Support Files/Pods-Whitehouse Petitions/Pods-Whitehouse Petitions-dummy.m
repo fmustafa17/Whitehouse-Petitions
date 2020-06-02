271428d38a003409d253864b8d3624855653a6e9
Farhana Mustafa <farhana.x.mustafa@kp.org>
2020-06-02 09:59:35 -0700

    adding firebase crashlytics

---
+++ Pods/Target Support Files/Pods-Whitehouse Petitions/Pods-Whitehouse Petitions-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Whitehouse_Petitions : NSObject
+@end
+@implementation PodsDummy_Pods_Whitehouse_Petitions
+@end
--- Whitehouse Petitions/ViewController.swift
+++ Whitehouse Petitions/ViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import FirebaseCrashlytics
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let button = UIButton(type: .roundedRect)
+            button.frame = CGRect(x: 20, y: 50, width: 100, height: 30)
+            button.setTitle("Crash", for: [])
+            button.addTarget(self, action: #selector(self.crashButtonTapped(_:)), for: .touchUpInside)
+            view.addSubview(button)
+        }
+
+    @IBAction func crashButtonTapped(_ sender: AnyObject) {
+        fatalError()
     }
 
 
