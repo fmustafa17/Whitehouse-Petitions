c29487b5bc119640e8acd104e74943adda0779c9
Farhana Mustafa <farhana_mustafa@intuit.com>
2020-12-08 13:56:49 -0500

    adding detail view controller

---
+++ Whitehouse Petitions/DetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  DetailViewController.swift
+//  Whitehouse Petitions
+//
+//  Created by fmustafa on 12/8/20.
+//  Copyright © 2020 Farhana Mustafa. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+    var webView: WKWebView!
+    var detailItem: Petition?
+    
+    override func loadView() {
+        webView = WKWebView()
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let detailItem = detailItem else { return }
+        let html = """
+                    <html>
+                    <head>
+                    <meta name="viewport" content="width=device-width, initialscale=1">
+                    <style> body { font-size: 150%; } </style>
+                    </head>
+                    <body>
+                    \(detailItem.body)
+                    </body>
+                    </html>
+                    """
+        webView.loadHTMLString(html, baseURL: nil)
+    }
+    
+}
